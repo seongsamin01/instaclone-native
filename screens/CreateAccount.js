559a221632ec235a1cb819be91f2a8a5ab19d342
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-24 17:37:12 +0900

    #14 Create Account 2 (인적사항란)

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { TextInput } from "react-native";
 import styled from "styled-components/native";
+import AuthButton from "../components/auth/AuthButton";
+import AuthLayout from "../components/auth/AuthLayout";
 
 const Container = styled.View`
   flex: 1;
@@ -7,5 +10,41 @@ const Container = styled.View`
 `;
 
 export default function CreateAccount() {
-    return <Container></Container>;
+    return (
+        <AuthLayout>
+          <TextInput
+            placeholder="First Name"
+            placeholderTextColor="gray"
+            returnKeyType="next"
+            style={{ backgroundColor: "white", width: "100%" }}
+          />
+          <TextInput
+            placeholder="Last Name"
+            placeholderTextColor="gray"
+            returnKeyType="next"
+            style={{ backgroundColor: "white", width: "100%" }}
+          />
+          <TextInput
+            placeholder="Username"
+            placeholderTextColor="gray"
+            returnKeyType="next"
+            style={{ backgroundColor: "white", width: "100%" }}
+          />
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor="gray"
+            keyboardType="email-address"
+            returnKeyType="next"
+            style={{ backgroundColor: "white", width: "100%" }}
+          />
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor="gray"
+            secureTextEntry
+            returnKeyType="done"
+            style={{ backgroundColor: "white", width: "100%" }}
+          />
+          <AuthButton text="Create Account" disabled={true} onPress={() => null} />
+        </AuthLayout>
+      );
   }
