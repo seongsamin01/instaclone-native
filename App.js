570f2de7d1d273ce91b2f6278bfbc2e0c227964c
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-24 16:31:16 +0900

    #14 Navigation (stack navigation)

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import AppLoading from "expo-app-loading";
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import * as Font from "expo-font";
-import { StyleSheet, Text, View } from "react-native";
+import { Asset } from "expo-asset";
+import LoggedOutNav from "./navigators/LoggedOutNav";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -23,18 +24,8 @@ export default function App() {
     );
   }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <LoggedOutNav />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
