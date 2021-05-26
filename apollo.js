f8e0eef5f12fa0df3f55ba8594ffb1e2cdc49d28
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-26 16:02:18 +0900

    #14 Recap (한번로그인해놓으면, 로그인상태저장/ AsyncStorage)

--- apollo.js
+++ apollo.js
@@ -2,13 +2,15 @@ import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const isLoggedInVar = makeVar(false);
+export const tokenVar = makeVar("");
 
 export const logUserIn = async (token) => {
     await AsyncStorage.multiSet([
-      ["token", JSON.stringify(token)],
-      ["loggedIn", JSON.stringify("yes")],
+      ["token", token],
+      ["loggedIn", "yes"],
     ]);
     isLoggedInVar(true);
+    tokenVar(token);
   };
 
 const client = new ApolloClient({
