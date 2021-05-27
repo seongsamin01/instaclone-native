ef0b5978400a84312521df33f6416efd5e9ee16d
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-27 16:31:50 +0900

    #15 Photo 2

--- components/Photo.js
+++ components/Photo.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { useNavigation } from "@react-navigation/native";
 import styled from "styled-components/native";
-import { useWindowDimensions } from "react-native";
+import { Image, useWindowDimensions } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Container = styled.View``;
-const Header = styled.View``;
-const UserAvatar = styled.Image``;
+const Header = styled.TouchableOpacity`
+  padding: 10px;
+  flex-direction: row;
+  align-items: center;
+`;
+const UserAvatar = styled.Image`
+  margin-right: 10px;
+  width: 25px;
+  height: 25px;
+  border-radius: 12.5;
+`;
 const Username = styled.Text`
   color: white;
+  font-weight: 600;
 `;
 const File = styled.Image``;
 const Actions = styled.View``;
@@ -21,43 +33,53 @@ const Likes = styled.Text`
 `;
 
 function Photo({ id, user, caption, file, isLiked, likes }) {
-    const { width, height } = useWindowDimensions();
-    return (
-      <Container>
-        <Header>
-          <UserAvatar />
+  const navigation = useNavigation();
+  const { width, height } = useWindowDimensions();
+  const [imageHeight, setImageHeight] = useState(height - 450);
+  useEffect(() => {
+    Image.getSize(file, (width, height) => {
+      setImageHeight(height / 3);
+    });
+  }, [file]);
+  return (
+    <Container>
+      <Header onPress={() => navigation.navigate("Profile")}>
+        <UserAvatar resizeMode="cover" source={{ uri: user.avatar }} />
+        <Username>{user.username}</Username>
+      </Header>
+      <File
+        resizeMode="cover"
+        style={{
+          width,
+          height: imageHeight,
+        }}
+        source={{ uri: file }}
+      />
+      <Actions>
+        <Action />
+        <Action />
+      </Actions>
+      <Likes>{likes === 1 ? "1 like" : `${likes} likes`}</Likes>
+      <Caption>
+        <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
           <Username>{user.username}</Username>
-        </Header>
-        <File
-          style={{
-            width,
-            height: height - 500,
-          }}
-          source={{ uri: file }}
-        />
-        <Actions>
-          <Action />
-          <Action />
-        </Actions>
-        <Likes>{likes === 1 ? "1 like" : `${likes} likes`}</Likes>
-        <Caption>
-          <Username>{user.username}</Username>
-          <CaptionText>{caption}</CaptionText>
-        </Caption>
-      </Container>
-    );
-  }
+        </TouchableOpacity>
+        <CaptionText>{caption}</CaptionText>
+      </Caption>
+    </Container>
+  );
+}
 
-  Photo.propTypes = {
-    id: PropTypes.number.isRequired,
-    user: PropTypes.shape({
-      avatar: PropTypes.string,
-      username: PropTypes.string.isRequired,
-    }),
-    caption: PropTypes.string,
-    file: PropTypes.string.isRequired,
-    isLiked: PropTypes.bool.isRequired,
-    likes: PropTypes.number.isRequired,
-    commentNumber: PropTypes.number.isRequired,
-  };
-  export default Photo;
+Photo.propTypes = {
+  id: PropTypes.number.isRequired,
+  user: PropTypes.shape({
+    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired,
+  }),
+  caption: PropTypes.string,
+  file: PropTypes.string.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  likes: PropTypes.number.isRequired,
+  commentNumber: PropTypes.number.isRequired,
+};
+export default Photo;
