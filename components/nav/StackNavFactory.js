3dcf3e56651750303c0d0f8a0096d5019dfe62be
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-27 14:59:22 +0900

    #15 Stack and Tabs 2 (navigation)

--- components/nav/StackNavFactory.js
+++ components/nav/StackNavFactory.js
@@ -10,20 +10,29 @@ import Me from "../../screens/Me";
 const Stack = createStackNavigator();
 
 export default function StackNavFactory({ screenName }) {
-    return (
-      <Stack.Navigator>
-        {screenName === "Feed" ? (
-          <Stack.Screen name={"Feed"} component={Feed} />
-        ) : null}
-        {screenName === "Search" ? (
-          <Stack.Screen name={"Search"} component={Search} />
-        ) : null}
-        {screenName === "Notifications" ? (
-          <Stack.Screen name={"Notifications"} component={Notifications} />
-        ) : null}
-        {screenName === "Me" ? <Stack.Screen name={"Me"} component={Me} /> : null}
-        <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="Photo" component={Photo} />
-      </Stack.Navigator>
-    );
-  }
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+        headerTintColor: "white",
+        headerStyle: {
+          shadowColor: "rgba(255, 255, 255, 0.3)",
+          backgroundColor: "black",
+        },
+      }}
+    >
+      {screenName === "Feed" ? (
+        <Stack.Screen name={"Feed"} component={Feed} />
+      ) : null}
+      {screenName === "Search" ? (
+        <Stack.Screen name={"Search"} component={Search} />
+      ) : null}
+      {screenName === "Notifications" ? (
+        <Stack.Screen name={"Notifications"} component={Notifications} />
+      ) : null}
+      {screenName === "Me" ? <Stack.Screen name={"Me"} component={Me} /> : null}
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Photo" component={Photo} />
+    </Stack.Navigator>
+  );
+}
