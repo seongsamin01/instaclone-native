2d3759a8056cd68e10b80e678218bbd3f7da24ae
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-28 20:29:05 +0900

    #17 Select Photo part 3

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 import * as MediaLibrary from "expo-media-library";
 import styled from "styled-components/native";
+import {
+  FlatList,
+  Image,
+  TouchableOpacity,
+  useWindowDimensions,
+} from "react-native";
 
 const Container = styled.View`
   flex: 1;
@@ -17,37 +24,73 @@ const Bottom = styled.View`
   background-color: black;
 `;
 
+const ImageContainer = styled.TouchableOpacity``;
+const IconContainer = styled.View`
+  position: absolute;
+  bottom: 5px;
+  right: 0px;
+`;
+
 export default function SelectPhoto() {
-    const [ok, setOk] = useState(false);
-    const [photos, setPhotos] = useState([]);
-    const getPhotos = async () => {
-      if (ok) {
-        const { assets: photos } = await MediaLibrary.getAssetsAsync();
-        setPhotos(photos);
-      }
-    };
-    const getPermissions = async () => {
-      const {
-        accessPrivileges,
-        canAskAgain,
-      } = await MediaLibrary.getPermissionsAsync();
-      if (accessPrivileges === "none" && canAskAgain) {
-        const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync();
-        if (accessPrivileges !== "none") {
-          setOk(true);
-        }
-      } else if (accessPrivileges !== "none") {
+  const [ok, setOk] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [chosenPhoto, setChosenPhoto] = useState("");
+  const getPhotos = async () => {
+    const { assets: photos } = await MediaLibrary.getAssetsAsync();
+    setPhotos(photos);
+    setChosenPhoto(photos[0]?.uri);
+  };
+  const getPermissions = async () => {
+    const { accessPrivileges, canAskAgain } =
+      await MediaLibrary.getPermissionsAsync();
+    if (accessPrivileges === "none" && canAskAgain) {
+      const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync();
+      if (accessPrivileges !== "none") {
         setOk(true);
+        getPhotos();
       }
-    };
-    useEffect(() => {
-      getPermissions();
+    } else if (accessPrivileges !== "none") {
+      setOk(true);
       getPhotos();
-    }, []);
-    return (
-      <Container>
-        <Top />
-        <Bottom></Bottom>
-      </Container>
-    );
-  }
+    }
+  };
+  useEffect(() => {
+    getPermissions();
+  }, []);
+  const numColumns = 4;
+  const { width } = useWindowDimensions();
+  const choosePhoto = (uri) => {
+    setChosenPhoto(uri);
+  };
+  const renderItem = ({ item: photo }) => (
+    <ImageContainer onPress={() => choosePhoto(photo.uri)}>
+      <Image
+        source={{ uri: photo.uri }}
+        style={{ width: width / numColumns, height: 100 }}
+      />
+      <IconContainer>
+        <Ionicons name="checkmark-circle" size={18} color="white" />
+      </IconContainer>
+    </ImageContainer>
+  );
+  return (
+    <Container>
+      <Top>
+        {chosenPhoto !== "" ? (
+          <Image
+            source={{ uri: chosenPhoto }}
+            style={{ width, height: "100%" }}
+          />
+        ) : null}
+      </Top>
+      <Bottom>
+        <FlatList
+          data={photos}
+          numColumns={numColumns}
+          keyExtractor={(photo) => photo.id}
+          renderItem={renderItem}
+        />
+      </Bottom>
+    </Container>
+  );
+}
