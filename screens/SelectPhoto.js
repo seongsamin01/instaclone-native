28b6c07f39abec33fa52944006e667e279160980
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-28 18:22:42 +0900

    #17 Select Photo part 1

---
+++ screens/SelectPhoto.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Top = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Bottom = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+export default function SelectPhoto() {
+    return (
+      <Container>
+        <Top />
+        <Bottom />
+      </Container>
+    );
+  }
