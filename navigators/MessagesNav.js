0ea400205a455eefa7c39fa180f83a53eaa9921b
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-29 17:34:13 +0900

    #18 채팅창 화면 part1

--- navigators/MessagesNav.js
+++ navigators/MessagesNav.js
@@ -7,21 +7,28 @@ import Rooms from "../screens/Rooms";
 const Stack = createStackNavigator();
 
 export default function MessagesNav() {
-    return (
-        <Stack.Navigator
-        screenOptions={{
-          headerTintColor: "white",
-          headerBackTitleVisible: false,
-          headerStyle: {
-            backgroundColor: "black",
-          },
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: "white",
+        headerBackTitleVisible: false,
+        headerStyle: {
+          borderBottomColor: "rgba(255, 255, 255, 0.3)",
+          shadowColor: "rgba(255, 255, 255, 0.3)",
+          backgroundColor: "black",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="Rooms"
+        options={{
           headerBackImage: ({ tintColor }) => (
-            <Ionicons color={tintColor} name="chevron-down" size={28} />
+            <Ionicons color={tintColor} name="chevron-down" size={30} />
           ),
         }}
-      >
-        <Stack.Screen name="Rooms" component={Rooms} />
-        <Stack.Screen name="Room" component={Room} />
-      </Stack.Navigator>
-    );
-  }
+        component={Rooms}
+      />
+      <Stack.Screen name="Room" component={Room} />
+    </Stack.Navigator>
+  );
+}
